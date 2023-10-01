cba21be301b7be24d153802ab3dd21b80154fa5e
ismagkar <ismagkar4@gmil.com>
2023-10-01 17:25:22 +0400

    return hosts env variable

--- admin_service/config/settings.py
+++ admin_service/config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", False) == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0" ,"localhost"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 ROOT_URLCONF = "config.urls"
 
